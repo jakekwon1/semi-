732de35824a8f56e982822612c220658ba1d0be2
jake <jakekwon99@gmail.com>
2023-02-28 14:20:36 +0900

    middle check

---
+++ include.py
@@ -0,0 +1,9 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot  as plt
+import matplotlib
+
+plt.rc('font', family='gulim') # For Windows
+matplotlib.rcParams['axes.unicode_minus'] = False 
+
